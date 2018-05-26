63f6947974d84540b8a05f8d9a2bb5526b134596
Scribee <38041006+Scribee@users.noreply.github.com>
2018-05-25 21:39:49 -0600

    Updated version number

--- src/main/java/scribee/cvcStats/Reference.java
+++ src/main/java/scribee/cvcStats/Reference.java
@@ -3,5 +3,5 @@ package scribee.cvcStats;
 public class Reference {
 	public static final String MODID = "cvcstats";
 	public static final String MOD_NAME = "CvcStats";
-	public static final String VERSION = "0.1.5";
+	public static final String VERSION = "0.1.6";
 }
